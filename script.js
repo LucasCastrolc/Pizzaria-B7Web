d4e37224bed74013d3332421d9de5fbfc5dd8782
Lucas Castro <allyson.01779@colegiopioxii.com.br>
2021-10-10 08:13:51 -0300

    update script.js

--- script.js
+++ script.js
@@ -22,6 +22,10 @@ pizzaJson.map((item, index)=>{
         dqs ('.pizzaBig img').src = pizzaJson[key].img;
         dqs('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         dqs('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        dqs('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        dqsall('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
 
         dqs ('.pizzaWindowArea').style.opacity = 0;
         dqs ('.pizzaWindowArea').style.display = 'flex';
